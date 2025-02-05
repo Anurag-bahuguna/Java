197b4607ba0110df544109db7bec3a0d5901afd6
Anurag-bahuguna <anurag.bahuguna777@gmail.com>
2025-02-05 23:57:12 +0530

    adding new files and manage all

---
+++ 2_Variable_DataType/input.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class input {
+    public static void main(String[] args) {
+        //input
+        Scanner sc= new Scanner(System.in);
+        String name = sc.nextLine();
+        int age = sc.nextInt();
+
+        System.out.println(name);
+        System.out.println(age);
+
+        //taking 2 numbers from user and adding them
+        System.out.println("Enter 2 numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum =a+b;
+        System.out.println("Sum of a and b is: "+sum);
+
+
+
+    }
+}
+
---
+++ 2_Variable_DataType/output.java
@@ -0,0 +1,7 @@
+public class output {
+    public static void main(String[] args){
+        System.out.print("my name is anurag\n");
+        System.out.println("learning java");
+        System.out.print("I am a student");
+    }
+}
