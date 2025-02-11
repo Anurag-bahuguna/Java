100acfc7c0a6cc24fe7654d32454562004e54045
Anurag-bahuguna <anurag.bahuguna777@gmail.com>
2025-02-11 21:00:44 +0530

    new files

--- Function/statics.java
+++ Function/statics.java
@@ -1,10 +1,11 @@
 public class statics {
+    // non static function
     void add(){
         System.out.println("add fun");
     }
-
+    // static function
     static void sub(){
-        System.out.println("aub fun");
+        System.out.println("sub fun");
     }
     public static void main(String[] args) {
         statics s = new statics();
