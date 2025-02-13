5cab24ce92c1322594a601ef40216d09e0876196
Anurag-bahuguna <anurag.bahuguna777@gmail.com>
2025-02-13 21:28:49 +0530

    pattern files & nasted loop

--- 3_CondtionalStatement_loops/pattern_Q1.java
+++ 3_CondtionalStatement_loops/pattern_Q1.java
@@ -46,20 +46,63 @@ public class pattern_Q1 {
         // }
 
     // wap to print half pyramid after 180 degree rotation
-        int n=4;
+        // int n=4;
 
         //outer loop
-        for(int i=1; i<=n; i++){
-            //inner loop
-            for(int j=1; j<n-i; j++){
-                System.out.println(" ");
-            }
+        // for(int i=1; i<=n; i++){
+        //     //inner loop
+        //     for(int j=1; j<n-i; j++){
+        //         System.out.println(" ");
+        //     }
 
-            //inner loop
-            for(int j=1; j<=i; j++){
-                System.out.print(" * ");
+        //     //inner loop
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print(" * ");
+        //     }
+        //     System.out.println();
+        // }
+
+    // wap to
+        // int n=5;
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print(j+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // int n=5;
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=n-i+1; j++){
+        //         System.out.print(j+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+    //Floyds trinagle
+    // int n=5;
+    // int number=1;
+    // for(int i=1; i<=n; i++){
+    //     for(int j=1; j<=i; j++){
+    //         System.out.print(number+" ");
+    //         number++;
+    //     }        
+    //     System.out.println();
+        
+    // }
+
+    // wap to print 0-1 triangle
+    int n=5;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            if((i+j)%2==0){
+                System.out.print("1 ");     //even
+            }else{
+                System.out.print("0 ");     //odd
             }
-            System.out.println();
         }
+        System.out.println();
+    }
+
     }
 }
