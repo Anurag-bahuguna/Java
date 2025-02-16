3c4490f5ab80c97567001a070a7b520b9474bff2
Anurag-bahuguna <anurag.bahuguna777@gmail.com>
2025-02-16 23:44:01 +0530

    new file added

---
+++ 3_operator/bit_4op.java
@@ -0,0 +1,10 @@
+public class bit_4op {
+    public static void main(String[] args) {
+        int a =4;
+        int b =5;
+
+        int x= (a|b);
+        // int x= (a&b);
+        System.out.println(x);
+    }
+}
