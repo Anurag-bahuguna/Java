852bc2534aded940d8d7a65bc464d4fd5e49a3b2
Anurag-bahuguna <anurag.bahuguna777@gmail.com>
2025-02-21 22:42:52 +0530

    Added new file

---
+++ Abstraction/abstraction_3.java
@@ -0,0 +1,12 @@
+package Java.Abstraction;
+
+abstract class demo_1{
+    static void tell(){
+        System.out.println("namaste");
+    };
+}
+public class abstraction_3 {
+    public static void main(String[] args) {
+        demo_1.tell();
+    }
+}
