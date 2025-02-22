f761237126b5e41430fdd747fb727ec7099d5536
Anurag-bahuguna <anurag.bahuguna777@gmail.com>
2025-02-22 22:35:55 +0530

    added new file

---
+++ Inheritance/derived_1.java
@@ -0,0 +1,15 @@
+package Java.Inheritance;
+
+class base {
+    base(){
+        System.out.println("in base");
+    }    
+}
+class derived_1 extends base{
+    derived_1(){
+        System.out.println("in derived");
+    }
+    public static void main(String[] agrs){
+        derived_1 d= new derived_1();
+    }
+}
