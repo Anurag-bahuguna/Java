297199c62c74e8a5af6301bbc08a03c2e89f6be1
Anurag-bahuguna <anurag.bahuguna777@gmail.com>
2025-02-22 22:37:26 +0530

    added new file

---
+++ Inheritance/derived_4.java
@@ -0,0 +1,29 @@
+package Java.Inheritance;
+
+
+
+class parent{
+    void show(){
+        System.out.println("in parent");
+    }
+}
+class child extends parent{
+    void display(){
+        System.out.println("in child");
+        super.show();
+    }
+    public class inner extends child{
+        void tell(){
+            System.out.println("in inner class");
+            super.display();
+        }
+    }
+}
+class derived_4 {
+    public static void main(String[] agrs){
+        child c = new child();
+        c.display();
+        child.inner i = c.new inner();
+        i.tell();
+    }
+}
