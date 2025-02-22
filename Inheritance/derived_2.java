39c271a8df4ac99f9de919eb3e0844558ff236ea
Anurag-bahuguna <anurag.bahuguna777@gmail.com>
2025-02-22 22:36:35 +0530

    added new file

---
+++ Inheritance/derived_2.java
@@ -0,0 +1,19 @@
+package Java.Inheritance;
+
+class base {
+    base(int x){
+        int num;
+        num=x;
+        System.out.println("in base");
+        System.out.println(num);
+    }    
+}
+class derived_2 extends base{
+    derived_2(){
+        super(10);
+        System.out.println("in derived");
+    }
+    public static void main(String[] agrs){
+        derived_2 d= new derived_2();
+    }
+}
