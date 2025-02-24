9ceffad18e05a32b412c4f1dc06d80784bd4e90f
Anurag-bahuguna <anurag.bahuguna777@gmail.com>
2025-02-25 00:18:33 +0530

    update

--- Polymorphism/poly_1.java
+++ Polymorphism/poly_1.java
@@ -1,5 +1,21 @@
-package Java.Polymorphism;
+// 1.overloading
+// 2.overriding
 
+// 2.
+package Java.Polymorphism;
+class A{
+    void show(){
+        System.out.println("hello from A");
+    }
+}
+class B extends A{
+    void show(int a){
+        System.out.println("hello from B");
+    }
+}
 public class poly_1 {
-    
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.show(10);
+    }
 }
