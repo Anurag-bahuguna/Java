c866b511aef03acd6adc6cd5b9bddffd005e4315
Anurag-bahuguna <anurag.bahuguna777@gmail.com>
2025-02-24 23:42:19 +0530

    updated

--- Function/statics.java
+++ Function/statics.java
@@ -1,4 +1,4 @@
-public class statics {
+class demo{
     // non static function
     void add(){
         System.out.println("add fun");
@@ -7,23 +7,32 @@ public class statics {
     static void sub(){
         System.out.println("sub fun");
     }
+}
+
+public class statics {
+
+    public void multi(){
+        System.out.println("multi fun");
+    }
+
+    static int a;   //static variable
+    public  static void div(){
+        a = 20;      //so static function can access static variable only
+        System.out.println("div fun");
+    }
 
     public static void main(String[] args) {
     
-    // why it error
-        // public void multi(){
-        //     System.out.println("multi fun");
-        // }
+        
+        demo s = new demo();
 
-        // public  static void div(){
-        //     System.out.println("div fun");
-        // }
-        statics s = new statics();
-        // statics s1 = new statics();
+        s.add();    // to call non static function
+        demo.sub(); // to call static function
 
-        s.add();
-        sub();
-        // s1.multi();
+        statics s1 = new statics();
+
+        s1.multi();
+        statics.div();
     }
 
     
