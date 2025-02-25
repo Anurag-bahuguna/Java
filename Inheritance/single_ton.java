259b9ab0e32876d3232437a3195247e71f5cef45
Anurag-bahuguna <anurag.bahuguna777@gmail.com>
2025-02-25 21:44:22 +0530

    new file

---
+++ Inheritance/single_ton.java
@@ -0,0 +1,30 @@
+package Java.Inheritance;
+// single ton class
+// single ton class is a class that can have only one object (an instance of the class) at a time.
+public class single_ton {
+    public static void main(String[] args) {
+        A obj = A.getInstance();
+        obj.i = 5;
+        obj.show();
+
+        // A obj1 = new A();
+        // obj1.i = 5;
+        // obj1.show();
+    }
+}
+// class A is single ton class
+class A{
+    int i;
+    private static A obj = new A();
+
+    private A(){
+        System.out.println("in const A");
+    }
+    public static A getInstance(){
+
+        return obj;
+    }
+    public void show(){
+        System.out.println("in show A" +i);
+    }
+}
