2342686d0e79f42a6874e59e1211599ec45e720c
Anurag-bahuguna <anurag.bahuguna777@gmail.com>
2025-03-10 00:12:57 +0530

    adding new files

--- Arrays/array_2.java
+++ Arrays/array_2.java
@@ -2,6 +2,13 @@ package Java.Arrays;
 
 public class array_2 {
     public static void main(String[] args) {
-        
+        int arr[] = new int[4];
+        for(int i = 0; i < 4; i++){
+            arr[i] = i+1;            
+        }
+        //array using enhanced for loop
+        for (int i : arr) {
+            System.out.println(i);
+        }
     }
 }
