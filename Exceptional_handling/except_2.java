e960b3d88a141e966a500870641f65108e3adf2e
Anurag-bahuguna <anurag.bahuguna777@gmail.com>
2025-03-13 00:47:13 +0530

    updating

--- Exceptional_handling/except_2.java
+++ Exceptional_handling/except_2.java
@@ -7,22 +7,48 @@ import java.io.InputStreamReader;
 
 public class except_2 {
     public static void main(String[] args) {
+        // try{
+        //     int i,j;
+        //     System.out.println("Enter the value of i and j");
+        //     InputStreamReader is = new InputStreamReader(System.in);
+        //     BufferedReader br = new BufferedReader(is);
+        //     i = Integer.parseInt(br.readLine());
+        //     j = Integer.parseInt(br.readLine());
+        //     int k =i+j;
+        //     System.out.println("Sum is "+k);
+            
+        // }
+        // catch(IOException e){
+        //     System.out.println("Error in input");
+        // }
+        // catch(NumberFormatException e){
+        //     System.out.println("Number format exception");
+        // }
+
+        BufferedReader br = null;
         try{
             int i,j;
             System.out.println("Enter the value of i and j");
-            InputStreamReader is = new InputStreamReader(System.in);
-            BufferedReader br = new BufferedReader(is);
+            
+            br = new BufferedReader(new InputStreamReader(System.in)); //anonymoud bufferedReader object
             i = Integer.parseInt(br.readLine());
             j = Integer.parseInt(br.readLine());
-            int k =i+j;
-            System.out.println("Sum is "+k);
-            
+            int k =i/j;
+            System.out.println("Sum is "+k);            
         }
         catch(IOException e){
-            System.out.println("Error in input");
+            System.out.println(e);
         }
-        catch(NumberFormatException e){
-            System.out.println("Number format exception");
+        catch(Exception e){
+            System.out.println("unknown exception");
+        }
+        finally{
+            try{
+                br.close();
+                System.out.println("resource released");
+            }catch(Exception e){
+                
+            }
         }
     }
 }
