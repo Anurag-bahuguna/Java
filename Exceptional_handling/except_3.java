b11718765720166c0b45950ea58b25d3c0e2fee2
Anurag-bahuguna <anurag.bahuguna777@gmail.com>
2025-03-13 00:47:56 +0530

    new file

---
+++ Exceptional_handling/except_3.java
@@ -0,0 +1,15 @@
+package Java.Exceptional_handling;
+//exception handling throw keyword
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class except_3 {
+    public static void main(String[] args) throws Exception {
+        String s = "";
+        System.out.println("Enter a string");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        s = br.readLine();
+        System.out.println(s);
+
+    }
+}
