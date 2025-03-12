b0977f4fe5d908b38fe40d3e467975a4682dab88
Anurag-bahuguna <anurag.bahuguna777@gmail.com>
2025-03-13 00:20:22 +0530

    new file

--- Exceptional_handling/except_2.java
+++ Exceptional_handling/except_2.java
@@ -1,5 +1,28 @@
 package Java.Exceptional_handling;
 
+//Exception handling checked exception bufferedReader
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class except_2 {
-    
+    public static void main(String[] args) {
+        try{
+            int i,j;
+            System.out.println("Enter the value of i and j");
+            InputStreamReader is = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(is);
+            i = Integer.parseInt(br.readLine());
+            j = Integer.parseInt(br.readLine());
+            int k =i+j;
+            System.out.println("Sum is "+k);
+            
+        }
+        catch(IOException e){
+            System.out.println("Error in input");
+        }
+        catch(NumberFormatException e){
+            System.out.println("Number format exception");
+        }
+    }
 }
