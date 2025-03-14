b0ca1e0ee73769662588076f7e49548a98c7f556
Anurag-bahuguna <anurag.bahuguna777@gmail.com>
2025-03-14 23:17:03 +0530

    updating

--- Multi_Threading/mt_1.java
+++ Multi_Threading/mt_1.java
@@ -5,22 +5,30 @@ public class mt_1 {
         A obj = new A();    //creating instance of class A and B
         B obj1 = new B();   
 
-        obj.show();         //calling show method of class A
-        obj1.show();        //calling show method of class B
+        obj.start();         //calling show method of class A
+        obj1.start();        //calling show method of class B
     }
 }
 
-class A{
+class A extends Thread{
     public void show(){
         for(int i=0; i<4; i++){
         System.out.println("hi");
+        try{Thread.sleep(1000);}catch(Exception e){}      //sleep method is used to pause the execution of the thread for a specific amount of time
         }
     }
+    public void run(){
+        show();
+    }
 }
-class B{
+class B extends Thread{
     public void show(){
         for(int i=0; i<4; i++){
             System.out.println("hello");
+            try{Thread.sleep(1000);}catch(Exception e){}
             }
+    }
+    public void run(){
+        show();
     }
 }
