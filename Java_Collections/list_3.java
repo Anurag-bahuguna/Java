491b1c68e887be53723f037827347faa5bc305db
Anurag-bahuguna <anurag.bahuguna777@gmail.com>
2025-03-15 21:29:58 +0530

    new file

---
+++ Java_Collections/list_3.java
@@ -0,0 +1,36 @@
+// package Java.Java_Collections;
+// import java.util.ArrayList;
+// import java.util.Collections;
+// import java.util.List;
+// import java.util.Comparator;
+
+// //Comparable interface
+// public class list_3 {
+//     public static void main(String[] args) {
+//         ArrayList<Emp> Emps = new ArrayList<>();
+
+//         Emps.add(new Emp(101, "Vijay"));
+//         Emps.add(new Emp(102, "Ajay"));
+//         Emps.add(new Emp(103, "Jai"));
+
+//         for(Emp e: Emps){
+//             System.out.println(e);
+//         }
+//     }
+// }
+
+// class Emp{
+//     int eid;
+//     String ename;
+
+//     public Emp(int id, String name){
+//         eid = id;
+//         ename = name;
+//     }
+
+//     public String toString(){
+//         return eid + " " + ename;
+//     }
+// }
+
+
