07d07ead5b728b6397538ac962eb877ad8a6706a
Anurag-bahuguna <anurag.bahuguna777@gmail.com>
2025-03-19 21:55:01 +0530

    new folder adding

---
+++ APPLETS/applet_1.java
@@ -0,0 +1,10 @@
+package Java.APPLETS;
+import java.applet.Applet;  
+import java.awt.Graphics;
+
+public class applet_1 {
+    public static void main(String[] args) {
+        //applet code 
+        
+    }
+}
